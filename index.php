d72ae218808882028ebbc7a7220a6ac483d05060
rcik09 <ricanator@gmail.com>
2021-03-02 13:38:46 -0500

    Update index.php
    Updated the index.php file that you created.

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<html>
+
+  <head>
+    <title>Title Goes Here</title>
+  </head>
+<body>
+
+  <h1>Header Goes Here </h1>
+  
 <?php
 
-echo "Hello World";
+echo "Hello World ";
 
 ?>
+  
+  </body>
+</html>
